71a82485452ba3fa3aa1c21d1522faa9412e14bb
Pedro Hurtado <pedrohurt@gmail.com>
2025-06-23 18:58:16 +0200

    Exception handler

--- src/main/java/com/example/demo/features/ingredients/queries/IngredientGet.java
+++ src/main/java/com/example/demo/features/ingredients/queries/IngredientGet.java
@@ -1,6 +1,6 @@
 package com.example.demo.features.ingredients.queries;
 
-import java.util.Optional;
+
 import java.util.UUID;
 
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.core.Get;
 import com.example.demo.domain.Ingredient;
-import com.example.demo.infraestructure.IngredientRepository;
 
 @Configuration
 public class IngredientGet {
@@ -19,19 +19,22 @@ public class IngredientGet {
     }
     @RestController
     public class Controller{
-        private final IngredientRepository repository;
-        public Controller(final IngredientRepository repository){
+        private final Get<Ingredient,UUID> repository;
+        public Controller(final Get<Ingredient,UUID> repository){
             this.repository = repository;
         }
         @GetMapping("/ingredients/{id}")
-        public ResponseEntity<?> handler(@PathVariable UUID id){
-            Optional<Ingredient> ingredient = repository.findById(id); 
-            if(ingredient.isPresent()){
-                var entity = ingredient.get();
-                var response = new Response(entity.getId(),entity.getName(), entity.getCost());
-                return ResponseEntity.ok(response);
-            }
-            return ResponseEntity.status(404).body(null);
+        public ResponseEntity<?> handler(@PathVariable UUID id){            
+            
+            var ingredient = repository.get(id);
+
+            var response = new Response(
+                ingredient.getId(),
+                ingredient.getName(), 
+                ingredient.getCost());
+
+            return ResponseEntity.ok(response);
+            
         }
 
     }
